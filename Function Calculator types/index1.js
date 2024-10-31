1073302346f3ad88714f34c18c70270b54f8b884
sStudent <junaidashraf2500@gmail.com>
2024-10-31 13:18:40 +0530

    Web dev Course

---
+++ Function Calculator types/index1.js
@@ -0,0 +1,12 @@
+function add(num1 ,num2){
+    return num1 + num2;
+}
+
+// 2 example
+function multiply(num1 ,num2){
+    return num1 * num2;
+}
+//3 example
+function calculator(num1 , num2, operator){
+return operator(num1 , num2);
+}
