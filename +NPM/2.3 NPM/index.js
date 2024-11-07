fb7d6bef5ddbfa5c1968e2e8cb81d80d87ff54bb
sStudent <junaidashraf2500@gmail.com>
2024-11-07 11:56:55 +0530

    Web dev course

--- +NPM/2.3 NPM/index.js
+++ +NPM/2.3 NPM/index.js
@@ -1,1 +1,12 @@
-var generateName = require('sillyname');
+// var generateName = require("sillyname");
+import generateName from "sillyName";
+
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+// const superheroes = require("superheroes");
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+console.log(`I am ${name}!`);
